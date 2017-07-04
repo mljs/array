ce0cd8dc80441e3b5967ca2939c17937dd8a3bd3
Jefferson Hernández <jeffersonh44@gmail.com>
2017-07-04 10:18:57 -0500

    feat(mean): mean function added

--- packages/array-mean/src/__tests__/test.js
+++ packages/array-mean/src/__tests__/test.js
@@ -6,8 +6,8 @@ describe('array-mean', () => {
         expect(mean([0])).toEqual(0);
         expect(mean([1])).toEqual(1);
         expect(mean([1, 2])).toEqual(1.5);
-        expect(mean([1, 2, 1])).toEqual(2);
-        expect(mean([3, 2, 1])).toEqual(3);
+        expect(mean([1, 2, 1])).toBeCloseTo(1.33, 0.001);
+        expect(mean([3, 2, 1])).toEqual(2);
     });
     it('should throw on invalid value', () => {
         expect(() => mean()).toThrow(/input must be an array/);
--- packages/array-mean/src/index.js
+++ packages/array-mean/src/index.js
@@ -7,8 +7,14 @@ export default function mean(input) {
     if (!Array.isArray(input)) {
         throw new Error('input must be an array');
     }
+
     var sum = 0;
     var l = input.length;
+
+    if (l === 0) {
+        return undefined;
+    }
+
     for (var i = 0; i < l; i++) {
         sum += input[i];
     }
