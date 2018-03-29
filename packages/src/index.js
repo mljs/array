054b9454f42316bed2bde9ddb91cb7c534c2b3d1
Luc Patiny <luc@patiny.com>
2018-03-29 13:09:40 +0200

    work on possible release of ml-array on lactame

---
+++ packages/src/index.js
@@ -0,0 +1,19 @@
+import min from './packages/array-min/src';
+import max from './packages/array-max/src';
+import mean from './packages/array-mean/src';
+import median from './packages/array-median/src';
+import mode from './packages/array-mode/src';
+import rescale from './packages/array-rescale/src';
+import sd from './packages/array-standard-deviation/src';
+import variance from './packages/array-ariance/src';
+
+
+export default {
+    min,
+    max,
+    median,
+    mean,
+    rescale,
+    sd,
+    mode
+}
