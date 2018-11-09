50f16bf02e0a0cf5f1b9c5288f393128d9a2f6e1
Gleb Mikheev <mikheev@betaagency.ru>
2018-11-09 16:43:48 +0300

    Added default arguments transpilation  (#9)
    transpile with preset-env and create 2 bundles (cjs/esm)

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,23 @@
-const config = {
-  input: 'src/index.js',
-  output: {
-    file: 'lib/index.js',
-    format: 'cjs'
-  },
-  external: ['median-quickselect']
-};
+import babel from 'rollup-plugin-babel';
 
-export default config;
+const formats = [
+  { format: 'cjs', file: 'lib/index.js' },
+  { format: 'esm', file: 'lib-es6/index.js' }
+];
+
+const config = (opts) => ({
+  input: 'src/index.js',
+  output: opts,
+  external: ['median-quickselect'],
+  plugins:
+    opts.format === 'esm'
+      ? [
+        babel({
+          exclude: 'node_modules/**',
+          presets: ['@babel/env']
+        })
+      ]
+      : []
+});
+
+export default formats.map(config);
