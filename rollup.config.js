cb78b9e5f580fbcfb14b9b7d4a8f9bb7830757da
Michaël Zasso <targos@protonmail.com>
2018-11-09 13:35:28 +0100

    cleanup + preset-env

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,23 @@
 import babel from 'rollup-plugin-babel';
 
 const formats = [
-  { format: 'cjs', file: 'lib/index.js', },
-  { format: 'esm', file: 'lib-esm/index.js' },
+  { format: 'cjs', file: 'lib/index.js' },
+  { format: 'esm', file: 'lib-es6/index.js' }
 ];
 
 const config = (opts) => ({
   input: 'src/index.js',
   output: opts,
   external: ['median-quickselect'],
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      plugins: ['transform-es2015-parameters']
-    })
-  ]
+  plugins:
+    opts.format === 'esm'
+      ? [
+        babel({
+          exclude: 'node_modules/**',
+          presets: ['@babel/env']
+        })
+      ]
+      : []
 });
 
 export default formats.map(config);
