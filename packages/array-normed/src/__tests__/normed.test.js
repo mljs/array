1293e76988483d7a0bda0f59db39d4f46388cf53
Luc Patiny <luc@patiny.com>
2019-08-23 21:59:48 +0200

    feat (normed): add sum and max algorithm

---
+++ packages/array-normed/src/__tests__/normed.test.js
@@ -0,0 +1,37 @@
+import norm from '..';
+
+describe('array-norm', () => {
+  it('should return the norm', () => {
+    expect(norm([0])).toStrictEqual([0]);
+    expect(norm([0, 0])).toStrictEqual([0, 0]);
+    expect(norm([-1, 1])).toStrictEqual([-0.5, 0.5]);
+    expect(norm([1, 3])).toStrictEqual([0.25, 0.75]);
+  });
+
+  it('should return the norm algorithm=absolute', () => {
+    expect(norm([0], { meethod: 'absolute' })).toStrictEqual([0]);
+    expect(norm([0, 0], { meethod: 'absolute' })).toStrictEqual([0, 0]);
+    expect(norm([-1, 1], { meethod: 'absolute' })).toStrictEqual([-0.5, 0.5]);
+    expect(norm([1, 3], { meethod: 'absolute' })).toStrictEqual([0.25, 0.75]);
+  });
+
+  it('should return the norm algorithm=max', () => {
+    expect(norm([0], { algorithm: 'max' })).toStrictEqual([0]);
+    expect(norm([0, 0], { algorithm: 'max' })).toStrictEqual([0, 0]);
+    expect(norm([-1, 1], { algorithm: 'max' })).toStrictEqual([-1, 1]);
+    expect(norm([1, 4], { algorithm: 'max' })).toStrictEqual([0.25, 1]);
+  });
+
+  it('should return the norm algorithm=sum', () => {
+    expect(norm([0], { algorithm: 'sum' })).toStrictEqual([0]);
+    expect(norm([0, 0], { algorithm: 'sum' })).toStrictEqual([0, 0]);
+    expect(norm([-1, 1], { algorithm: 'sum' })).toStrictEqual([-1, 1]);
+    expect(norm([-1, 3], { algorithm: 'sum' })).toStrictEqual([-0.5, 1.5]);
+    expect(norm([1, 3], { algorithm: 'sum' })).toStrictEqual([0.25, 0.75]);
+  });
+
+  it('should throw on invalid value', () => {
+    expect(() => norm()).toThrow(/input must be an array/);
+    expect(() => norm([])).toThrow(/input must not be empty/);
+  });
+});
--- packages/array-normed/src/index.js
+++ packages/array-normed/src/index.js
@@ -1,8 +1,11 @@
+import max from 'ml-array-max';
+import sum from 'ml-array-sum';
+
 /**
  * Computes the norm of the given values
  * @param {Array<number>} input
  * @param {object} [options={}]
- * @param {string} [options.algorithm='absolute'] 
+ * @param {string} [options.algorithm='absolute'] absolute, sum or max
  * @return {number}
  */
 export default function norm(input, options = {}) {
@@ -16,10 +19,22 @@ export default function norm(input, options = {}) {
   }
 
   switch (algorithm.toLowerCase()) {
-    case 'absolute':
-      var sum = absoluteSum(input);
-      if (sum === 0) return input.slice(0);
-      return input.map((element) => element / sum);
+    case 'absolute': {
+      let absoluteSumValue = absoluteSum(input);
+      if (absoluteSumValue === 0) return input.slice(0);
+      return input.map((element) => element / absoluteSumValue);
+    }
+    case 'max': {
+      let maxValue = max(input);
+      if (maxValue === 0) return input.slice(0);
+      return input.map((element) => element / maxValue);
+    }
+    case 'sum': {
+      let sumValue = sum(input);
+      if (sumValue === 0) return input.slice(0);
+      return input.map((element) => element / sumValue);
+    }
+
     default:
       throw new Error(`norm: unknown algorithm: ${algorithm}`);
   }
