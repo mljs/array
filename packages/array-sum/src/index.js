54cca6dcf4d6dbaf8e9c1cd6bb387f0720c64d87
Luc Patiny <luc@patiny.com>
2019-08-23 21:44:51 +0200

    Add array-sum

---
+++ packages/array-sum/src/index.js
@@ -0,0 +1,22 @@
+import isArray from 'is-any-array';
+
+/**
+ * Computes the mean of the given values
+ * @param {Array<number>} input
+ * @return {number}
+ */
+export default function sum(input) {
+  if (!isArray(input)) {
+    throw new TypeError('input must be an array');
+  }
+
+  if (input.length === 0) {
+    throw new TypeError('input must not be empty');
+  }
+
+  var sum = 0;
+  for (var i = 0; i < input.length; i++) {
+    sum += input[i];
+  }
+  return sum;
+}
