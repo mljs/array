6bc77bf11cbb5ea3cc4ad391ccaeae271eb975ab
Kevin <kevin.jablonka@epfl.ch>
2020-10-05 23:02:58 +0200

    feat: Adding from/to options for min and max.
    - incorporating feedback from https://github.com/mljs/array/pull/11

--- packages/array-max/src/__tests__/max.test.js
+++ packages/array-max/src/__tests__/max.test.js
@@ -12,10 +12,38 @@ describe('array-max', () => {
     expect(max([1, 2])).toBe(2);
     expect(max([1, 2, 1])).toBe(2);
     expect(max([3, 2, 1])).toBe(3);
+    expect(max([3, 2, 1], { fromIndex: 0, toIndex: 2 })).toBe(3);
+    expect(max([3, 2, 1], { fromIndex: 0, toIndex: 3 })).toBe(3);
+    expect(max([3, 2, 1], { fromIndex: 1, toIndex: 3 })).toBe(2);
+    expect(max([3, 2, 1], { fromIndex: 0, toIndex: 2 })).toBe(3);
+    expect(max([3, 2, 1], { fromIndex: 2, toIndex: 3 })).toBe(1);
     expect(max(typedArray)).toBe(3);
+    expect(max(typedArray, { fromIndex: 0, toIndex: 2 })).toBe(2);
+    expect(max(typedArray, { fromIndex: 0, toIndex: 3 })).toBe(3);
   });
   it('should throw on invalid value', () => {
     expect(() => max()).toThrow(/input must be an array/);
     expect(() => max([])).toThrow(/input must not be empty/);
+    expect(() => max([1, 2, 3], { fromIndex: -1, toIndex: 2 })).toThrow(
+      /fromIndex must be a positive integer smaller than length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 4, toIndex: 2 })).toThrow(
+      /fromIndex must be a positive integer smaller than length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 3, toIndex: 3 })).toThrow(
+      /fromIndex must be a positive integer smaller than length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 1, toIndex: 0 })).toThrow(
+      /toIndex must be an integer greater than fromIndex and at most equal to length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 1, toIndex: 4 })).toThrow(
+      /toIndex must be an integer greater than fromIndex and at most equal to length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 0, toIndex: 1.5 })).toThrow(
+      /toIndex must be an integer greater than fromIndex and at most equal to length/,
+    );
+    expect(() => max([1, 2, 3], { fromIndex: 1.5, toIndex: 2 })).toThrow(
+      /fromIndex must be a positive integer smaller than length/,
+    );
   });
 });
