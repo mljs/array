7921ef54c9aa4ee5e4ebcfe4e4b25b3047bad448
Michaël Zasso <targos@protonmail.com>
2020-10-07 08:40:24 +0200

    chore: reformat files with Prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import min from '../packages/array-min/src';
 import max from '../packages/array-max/src';
 import mean from '../packages/array-mean/src';
 import median from '../packages/array-median/src';
+import min from '../packages/array-min/src';
 import mode from '../packages/array-mode/src';
+import normed from '../packages/array-normed';
 import rescale from '../packages/array-rescale/src';
+import sequentialFill from '../packages/array-sequential-fill/src';
 import sd from '../packages/array-standard-deviation/src';
 import sum from '../packages/array-sum/src';
 import variance from '../packages/array-variance/src';
-import sequentialFill from '../packages/array-sequential-fill/src';
-import normed from '../packages/array-normed';
 
 export {
   min,
@@ -21,5 +21,5 @@ export {
   sd,
   sequentialFill,
   sum,
-  variance
+  variance,
 };
